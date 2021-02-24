2531f4aef8b48e9bd42a63a93a39e1c06b13bc24
Patrizio Onida <patriziosimone@hotmail.it>
2021-02-24 13:33:03 +0100

    autosetup rebuild

--- AutoSetup.py
+++ AutoSetup.py
@@ -1,22 +1,52 @@
-Deps = ['googleapi', 'pytube']
 from subprocess import check_output as Shell
 
-if __name__ == '__main__':
-    CEnd = '\033[0m'
-    ErrorText = lambda S: '\33[31m' + S + CEnd
-    WarningText = lambda S: '\33[33m' + S + CEnd
+Dependencies = {'googleAPI' : 'googleAPI'}
 
+CEnd = '\033[0m'
+ErrorText = lambda S: '\33[31m' + S + CEnd
+WarningText = lambda S: '\33[33m' + S + CEnd
+OKText = lambda S: '\33[92m' + S + CEnd
+
+def CheckDependencies():
     ModulesList= [i.decode() for i in Shell('pip list', shell = True).split()][4 :]
     Modules = [ModulesList[i] for i in range(len(ModulesList)) if i % 2 == 0]
     Versions = [ModulesList[i] for i in range(len(ModulesList)) if i % 2 == 1]
     ModuleString = ' '.join([i.replace('-', '') + '==' + j for i,j in zip(Modules,Versions)])
 
-    for Dep in Deps:
-        if not Dep in ModuleString:
-            print(ErrorText('it seems that you have not the' + Dep + 'dependency in this python version'))
-            Shell('pip install ' + Dep)
+    MissingModules = [Dependency for Dependency in Dependencies if Dependencies[Dependency] not in ModuleString]
 
-            if not Dep in ModuleString:
-                print(ErrorText('you do not have required permission to install the module ' + Dep))
-                print(WarningText('try execute this script as admin'), '\n')
-                break
+    return MissingModules
+
+if __name__ == '__main__':
+    ModulesToIstall = CheckDependencies()
+    if len(ModulesToIstall) > 0 :
+        print(WarningText('you need to install the following modules:'))
+
+        for Module in ModulesToIstall:
+            print(ErrorText(Module))
+
+        UInput = input('do you want to autoinstall them? <y/n>')
+        Commands = ['pip install ' + Module for Module in ModulesToIstall]
+
+        if UInput.lower() == 'y':
+            for Command in Commands:
+                Shell(Command, shell = True)
+
+            print(WarningText('It SHOULD be all set'))
+            print(OKText('Restart this program to test it out'))
+            print(WarningText("(Try running as Administrator if it doesn't work)"))
+
+        elif UInput.lower() == 'n':
+            print(WarningText('copy and paste the following commands in cmd/powershell/terminal'))
+
+            for Command in Commands:
+                print(Command)
+
+        else:
+            print(ErrorText('invalid imput'))
+            print(ErrorText('exiting'))
+            quit()
+
+    else:
+        print(OKText('All set you are ready to start'))
+
