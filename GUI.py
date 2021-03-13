36bf9b1d2be5672d0e48e7af4891e90ca3570307
Patrizio Onida <patriziosimone@hotmail.it>
2021-03-13 18:25:25 +0100

    removed local media dependencies and completed class NavigationBar (and commented), placed correctly and tested (additional test needs to be done)

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 from math import ceil as Ceil
+from PIL.Image import new as Gen
 from PIL.ImageTk import PhotoImage as PilImg
 from tkinter import Tk as App
+from tkinter import Button as Button
 from tkinter import Label as Label
 from tkinter import Frame as Page
 from tkinter import PhotoImage as Photo
@@ -9,46 +11,62 @@ from time import sleep as Sleep
 from urllib.request import urlopen as URL
 
 class Uniflix(App): # main class of the app
+    Name = 'UNIFLIX'
+    Colors =\
+    {
+        'bg' : '#000000',
+        'fg' : '#E50914',
+        'text' : '#FFFFFF'
+    }#color of the app
+
     def __init__(self,  *args, **kwargs):
         App.__init__(self, *args, **kwargs)
         self.Title = 'Uniflix' #title of the app
-        self.Icon = Photo(file = 'Res/SapLogo.png') #logo
+        Logo = URL('https://upload.wikimedia.org/wikipedia/en/4/45/Sapienza_University_of_Rome.png').read() #raw data of the logo
+        self.Icon = PilImg(data = Logo) #logo
         self.iconphoto(True, self.Icon) #window icon
         self.title(self.Title)
-        self.geometry('1280x720+0+0')
-        #self.wm_state('zoomed')
+        #self.geometry('1280x720+800+0')
+        self.wm_state('zoomed')
 
         self.Screens =\
         {
-            Start().Name : Start(self)
+            Start.Name : Start(self),
+            Home.Name : Home(self)
         } #all pages of the app
 
         for Screen in self.Screens.values(): #placing every page with max dimentsion
             Screen.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
+            self.update()
 
         self.Raise('Start') #raise the start page
         self.Screens['Start'].Animate() #start the animation of start page
+        self.Raise('Home')
+        self.Screens['Home'].PlaceMoreWidgets()
 
     def Raise(self, Screen = str()): #this function raise the selected frame to the top
         self.Screens[Screen].tkraise()
 
-class Start(Page): #class for the starting animation page
-    def __init__(self, Parent = None,  *args, **kwargs):
-        self.Parent = Parent #setting parent attribute
-        self.Name = 'Start' #setting name
-        super().__init__(Parent, *args, **kwargs) #calling the init method of super class page
-        self.bg = '#000000' #background
-        self.fg = '#E50914' #forground
-        self.config(bg = self.bg) #setting the background
-        self.Font = lambda x : ('Bebas Neue ', x) #font of the page
+    def LogoFont(Dim):
+        return ('Bebas Neue ', Dim) #font of the page
 
-        self.Animrames = ['UNIFLIX'[: i] for i in range(1, len('Uniflix') + 1)]
+class Start(Page): #class for the starting animation page
+    Name = 'Start' #setting name
+
+    def __init__(self, Parent = None,  *args, **kwargs):
+        super().__init__(Parent, *args, **kwargs) #calling the init method of super class page
+        self.Parent = Parent #setting parent attribute
+        self.bg = Parent.Colors['bg'] #background
+        self.fg = Parent.Colors['fg'] #foreground
+        self.config(bg = self.bg) #setting the background
+
+        self.Animrames = [Uniflix.Name[: i] for i in range(1, len(Uniflix.Name) + 1)]
         self.LabelText = VarString() #variable for changing animation text
         self.LabelText.set(self.Animrames[0]) #setting the variable as the initial frame
 
         self.LogoLebel = Label(self) #creating a logo (letters)
         self.LogoLebel.config(bg = self.bg) #setting tthe background to match the frame
-        self.LogoLebel.config(font = self.Font(72)) #setting the font size (72 pt)
+        self.LogoLebel.config(font = Uniflix.LogoFont(72)) #setting the font size (72 pt)
         self.LogoLebel.config(textvariable = self.LabelText) #setting the text of the label as avariable text
         self.LogoLebel.place \
         (
@@ -91,6 +109,7 @@ class Start(Page): #class for the starting animation page
 
         #print(self.Name + ' fanding in') #debug
         StartColor = self.bg #setting the starting color
+        self.LogoLebel.config(fg = StartColor)
         for i in range(int(60)): #for loop(1 second) for the start frame to fade in
             StartColor = HexFade(StartColor, Fade, self.fg) #updating the values
             self.LogoLebel.config(fg = StartColor) #updating the forground color
@@ -98,12 +117,12 @@ class Start(Page): #class for the starting animation page
             Sleep(FrameTime) #waitng "a frame"
 
         #print(self.Name + ' stand time') #debug
-        for i in range(int(30)): #for loop (1/2 second) for start frame to stand still
+        for i in range(int(10)): #for loop (1/4 second) for start frame to stand still
             self.LogoLebel.config(fg = self.fg) #setting the foreground
             self.update() #updating the frame
             Sleep(FrameTime) #waitng "a frame"
 
-        print('writing')
+        #print('writing')
         for Frame in self.Animrames:
             self.LabelText.set(Frame)
             self.update() #updating the frame
@@ -111,18 +130,86 @@ class Start(Page): #class for the starting animation page
 
         #print(self.Name + ' fanding out') #debug
         EndColor = self.fg #staring color
+        self.LogoLebel.config(fg = EndColor)
         for i in range(int(30)): #for loop (1/2 second) for the logo to fade out
             EndColor = HexFade(EndColor, Fade, self.bg) #updating the value
             self.LogoLebel.config(fg = EndColor) #updating the foregroung color
             self.update() #updating the frame
             Sleep(FrameTime) #waitng "a frame"
 
+class NavigationBar(Page): #class of top navigation bar (menu button, logo, search button)
+    Name = 'NavigationBar'
+
+    def __init__(self, Parent, *args, **kwargs):
+        super().__init__(Parent, *args, **kwargs) #super class constructor method
+        self.Parent = Parent #setting the parent of the bar
+        self.bg = Uniflix.Colors['bg'] #setting background color as attribute
+        self.fg = Uniflix.Colors['fg'] #setting foreground color as attribute
+        self.config(bg = self.bg) #changing the background color
+
+        self.Menu = Button(self) #generating a button to trigger side menu
+        self.Menu.config(borderwidth = 0) #setting the button with no border
+        self.Menu.place \
+        (
+            anchor = 'nw',
+            relx = 0,
+            rely = 0
+        ) #placing the sidemenu button top left
+
+        self.Search = Button(self) #generating the button of the search
+        self.Search.config(borderwidth = 0) #setting the button with no border
+        self.Search.place \
+        (
+            anchor = 'ne',
+            relx = 1,
+            rely = 0
+        ) #placing the search button top right
+
+    def PlaceMoreWidgets(self): #this method generate and place additional widgets that can not be placed or completely used in the costructor
+        self.MenuIMG = PilImg(Gen('RGB', (self.winfo_height() - 10, self.winfo_height() - 10), (255, 0, 0))) #generating a square image as bg of the button #temporary
+        self.Menu.config(image = self.MenuIMG) #setting the image as button background
+
+        self.LogoLabel = Label(self) #generating the logo
+        self.LogoLabel.config(bg = self.bg) #setting the background to match with the frame
+        self.LogoLabel.config(fg = self.fg) #setting the foreground to match with the frame
+        self.LogoLabel.config(font = Uniflix.LogoFont(36)) #setting the font for the logo and the size
+        self.LogoLabel.config(text = Uniflix.Name) #setting the text of the logo as the name of the app
+        self.LogoLabel.place \
+        (
+            anchor = 'nw',
+            x = self.MenuIMG.width() + 10,
+            rely = 0,
+            relheight = 1
+        ) #placing the logo right after the menu button
+
+        self.SearchIMG = PilImg(Gen('RGB',(self.winfo_height() - 10, self.winfo_height() - 10), (0, 0, 255))) #generating a square image as bg of the button #temporary
+        self.Search.config(image = self.SearchIMG) #setting the image as button background
+
 class Home(Page): #main class of the main page of the app
-    def __init__(self,  *args, **kwargs):
-        self.Parent = Parent #setting parent attribute
-        self.Name = 'Home' #setting name
+    Name = 'Home' #setting name
+    def __init__(self, Parent = None,  *args, **kwargs):
         super().__init__(Parent, *args, **kwargs) #calling the init method of super class page
-        self.bg = '#000000' #background
+        self.Parent = Parent #setting parent attribute
+        self.bg = Parent.Colors['bg'] #background
+        self.config(bg = self.bg)
+        self.fg = Parent.Colors['fg'] #foreground
+        self.Text = Parent.Colors['text'] #text color
+
+        self.NavBar = NavigationBar(self) #genrating navigation bar (and its widget)
+        self.NavBar.place \
+        (
+            anchor = 'n',
+            relx = 0.5,
+            rely = 0,
+            relwidth = 1,
+            relheight = 0.06
+        ) #placing the bar on top
+
+    def PlaceMoreWidgets(self): #this function manage all the additional widgets of the page
+        self.Parent.update() #updating the window
+        self.NavBar.PlaceMoreWidgets() #placing the additiona widget of NavBar
+        self.Parent.update() #updating the window
+
 
 
 if __name__ == '__main__':
