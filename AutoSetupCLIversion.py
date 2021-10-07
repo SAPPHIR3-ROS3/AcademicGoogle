06e1494220a1ee1046a68fb4b199802f39eb3fc6
Patrizio Onida <patriziosimone@hotmail.it>
2021-10-08 01:38:06 +0200

    included DB part in the setup

--- AutoSetupCLIversion.py
+++ AutoSetupCLIversion.py
@@ -1,5 +1,6 @@
 from subprocess import check_output as Shell
-
+from os import name as SysName
+from os import system as Sys
 Dependencies = {'googleAPI' : 'googleAPI'} # dependencies need to run the program
 
 CEnd = '\033[0m' # end of coloured text
@@ -19,28 +20,44 @@ def CheckDependencies(): # function to check all the needd dependencies in the s
 
 if __name__ == '__main__':
     ModulesToIstall = CheckDependencies()
+    Sys('cls' if  SysName =='nt' else 'clear')
+
     if len(ModulesToIstall) > 0 :
         print(WarningText('you need to install the following modules:'))
 
         for Module in ModulesToIstall:
             print(ErrorText(Module))
 
-        UInput = input('do you want to autoinstall them? <y/n>')
+        UInput = input('do you want to autoinstall them? <y/n> ')
         Commands = ['pip install ' + Module for Module in ModulesToIstall]
 
         if UInput.lower() == 'y':
             for Command in Commands:
                 Shell(Command, shell = True)
 
-            print(WarningText('It SHOULD be all set'))
-            print(OKText('Restart this program to test it out'))
-            print(WarningText("(Try running as Administrator if tha script did not work properly)"))
-
         elif UInput.lower() == 'n':
             print(WarningText('copy and paste the following commands in cmd/powershell/terminal'))
 
             for Command in Commands:
                 print(Command)
+
+        else:
+            print(ErrorText('invalid imput'))
+            print(ErrorText('exiting'))
+            quit()
+
+        UInput = input('do you want to create a local database (faster research more memorry required)? <y/n> ')
+
+        if UInput.lower() == 'y':
+            from DBManagement import CreateDatabase
+            CreateDatabase()
+
+            print(WarningText('It SHOULD be all set'))
+            print(OKText('Restart this program to test it out'))
+            print(WarningText("(Try running as Administrator if tha script did not work properly)"))
+
+        elif UInput.lower() == 'n':
+            print('you can create database anytime with "python DBManagement.py -c" or "python3 DBManagement.py -c"')
 
         else:
             print(ErrorText('invalid imput'))
